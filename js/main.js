147b11a761ef34ef319432d30a927fb77c46f8e7
Shay Cojocaru <shayc@outlook.com>
2018-09-30 04:14:36 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-(function() {})();
+(function() {
+  "use strict";
+  const navToggle = document.querySelector(".nav-toggle");
+  const openClassName = "nav-open";
+
+  const toggleNav = () => document.body.classList.toggle(openClassName);
+
+  navToggle.addEventListener("click", () => {
+    toggleNav();
+  });
+})();
